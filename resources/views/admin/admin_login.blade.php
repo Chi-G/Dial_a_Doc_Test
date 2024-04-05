5b2d720149e5931ed62b2f04a8f50ef69c3ae80c
Chijindu Nwokeohuru <cnwokeohuru@avohealth.org>
2024-04-05 22:48:34 +0100

    login UI fix

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -101,218 +101,13 @@
         <div class="off-canvas-menu-close main-menu-close icon-element icon-element-sm shadow-sm" data-toggle="tooltip" data-placement="left" title="Close menu">
             <i class="la la-times"></i>
         </div><!-- end off-canvas-menu-close -->
-        <ul class="generic-list-item off-canvas-menu-list pt-90px">
-            <li>
-                <a href="#">Home</a>
-                <ul class="sub-menu">
-                    <li><a href="index.html">Home One</a></li>
-                    <li><a href="home-2.html">Home Two</a></li>
-                    <li><a href="home-3.html">Home Three</a></li>
-                    <li><a href="home-4.html">Home four</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">courses</a>
-                <ul class="sub-menu">
-                    <li><a href="course-grid.html">course grid</a></li>
-                    <li><a href="course-list.html">course list</a></li>
-                    <li><a href="course-grid-left-sidebar.html">grid left sidebar</a></li>
-                    <li><a href="course-grid-right-sidebar.html">grid right sidebar</a></li>
-                    <li><a href="course-list-left-sidebar.html">list left sidebar <span class="ribbon ribbon-blue-bg">New</span></a></li>
-                    <li><a href="course-list-right-sidebar.html">list right sidebar <span class="ribbon ribbon-blue-bg">New</span></a></li>
-                    <li><a href="course-details.html">course details</a></li>
-                    <li><a href="lesson-details.html">lesson details</a></li>
-                    <li><a href="my-courses.html">My courses</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Student</a>
-                <ul class="sub-menu">
-                    <li><a href="student-detail.html">student detail</a></li>
-                    <li><a href="student-quiz.html">take quiz</a></li>
-                    <li><a href="student-quiz-results.html">quiz results</a></li>
-                    <li><a href="student-quiz-result-details.html">quiz details</a></li>
-                    <li><a href="student-quiz-result-details-2.html">quiz details 2</a></li>
-                    <li><a href="student-path.html">path details</a></li>
-                    <li><a href="student-path-assessment.html">Skill Assessment</a></li>
-                    <li><a href="student-path-assessment-result.html">Skill result</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">pages</a>
-                <ul class="sub-menu">
-                    <li><a href="dashboard.html">dashboard <span class="ribbon">Hot</span></a></li>
-                    <li><a href="about.html">about</a></li>
-                    <li><a href="teachers.html">Teachers</a></li>
-                    <li><a href="teacher-detail.html">Teacher detail</a></li>
-                    <li><a href="careers.html">careers</a></li>
-                    <li><a href="career-details.html">career details</a></li>
-                    <li><a href="categories.html">categories</a></li>
-                    <li><a href="#">Terms & conditions</a></li>
-                    <li><a href="privacy-policy.html">privacy policy</a></li>
-                    <li><a href="for-business.html">for business</a></li>
-                    <li><a href="become-a-teacher.html">become an instructor</a></li>
-                    <li><a href="faq.html">FAQs</a></li>
-                    <li><a href="admission.html">admission</a></li>
-                    <li><a href="gallery.html">gallery</a></li>
-                    <li><a href="pricing-table.html">pricing tables</a></li>
-                    <li><a href="contact.html">contact</a></li>
-                    <li><a href="sign-up.html">sign-up</a></li>
-                    <li><a href="login.html">login</a></li>
-                    <li><a href="recover.html">recover</a></li>
-                    <li><a href="shopping-cart.html">cart</a></li>
-                    <li><a href="checkout.html">checkout</a></li>
-                    <li><a href="error.html">page 404</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">blog</a>
-                <ul class="sub-menu">
-                    <li><a href="blog-full-width.html">blog full width </a></li>
-                    <li><a href="blog-no-sidebar.html">blog no sidebar</a></li>
-                    <li><a href="blog-left-sidebar.html">blog left sidebar</a></li>
-                    <li><a href="blog-right-sidebar.html">blog right sidebar</a></li>
-                    <li><a href="blog-single.html">blog detail</a></li>
-                </ul>
-            </li>
-        </ul>
     </div><!-- end off-canvas-menu -->
     <div class="off-canvas-menu custom-scrollbar-styled category-off-canvas-menu">
         <div class="off-canvas-menu-close cat-menu-close icon-element icon-element-sm shadow-sm" data-toggle="tooltip" data-placement="left" title="Close menu">
             <i class="la la-times"></i>
         </div><!-- end off-canvas-menu-close -->
-        <ul class="generic-list-item off-canvas-menu-list pt-90px">
-            <li>
-                <a href="course-grid.html">Development</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All Development</a></li>
-                    <li><a href="#">Web Development</a></li>
-                    <li><a href="#">Mobile Apps</a></li>
-                    <li><a href="#">Game Development</a></li>
-                    <li><a href="#">Databases</a></li>
-                    <li><a href="#">Programming Languages</a></li>
-                    <li><a href="#">Software Testing</a></li>
-                    <li><a href="#">Software Engineering</a></li>
-                    <li><a href="#">E-Commerce</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">business</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All Business</a></li>
-                    <li><a href="#">Finance</a></li>
-                    <li><a href="#">Entrepreneurship</a></li>
-                    <li><a href="#">Strategy</a></li>
-                    <li><a href="#">Real Estate</a></li>
-                    <li><a href="#">Home Business</a></li>
-                    <li><a href="#">Communications</a></li>
-                    <li><a href="#">Industry</a></li>
-                    <li><a href="#">Other</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">IT & Software</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All IT & Software</a></li>
-                    <li><a href="#">IT Certification</a></li>
-                    <li><a href="#">Hardware</a></li>
-                    <li><a href="#">Network & Security</a></li>
-                    <li><a href="#">Operating Systems</a></li>
-                    <li><a href="#">Other</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">Finance & Accounting</a>
-                <ul class="sub-menu">
-                    <li><a href="#"> All Finance & Accounting</a></li>
-                    <li><a href="#">Accounting & Bookkeeping</a></li>
-                    <li><a href="#">Cryptocurrency & Blockchain</a></li>
-                    <li><a href="#">Economics</a></li>
-                    <li><a href="#">Investing & Trading</a></li>
-                    <li><a href="#">Other Finance & Economics</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">design</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All Design</a></li>
-                    <li><a href="#">Graphic Design</a></li>
-                    <li><a href="#">Web Design</a></li>
-                    <li><a href="#">Design Tools</a></li>
-                    <li><a href="#">3D & Animation</a></li>
-                    <li><a href="#">User Experience</a></li>
-                    <li><a href="#">Other</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">Personal Development</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All Personal Development</a></li>
-                    <li><a href="#">Personal Transformation</a></li>
-                    <li><a href="#">Productivity</a></li>
-                    <li><a href="#">Leadership</a></li>
-                    <li><a href="#">Personal Finance</a></li>
-                    <li><a href="#">Career Development</a></li>
-                    <li><a href="#">Parenting & Relationships</a></li>
-                    <li><a href="#">Happiness</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">Marketing</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All Marketing</a></li>
-                    <li><a href="#">Digital Marketing</a></li>
-                    <li><a href="#">Search Engine Optimization</a></li>
-                    <li><a href="#">Social Media Marketing</a></li>
-                    <li><a href="#">Branding</a></li>
-                    <li><a href="#">Video & Mobile Marketing</a></li>
-                    <li><a href="#">Affiliate Marketing</a></li>
-                    <li><a href="#">Growth Hacking</a></li>
-                    <li><a href="#">Other</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">Health & Fitness</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All Health & Fitness</a></li>
-                    <li><a href="#">Fitness</a></li>
-                    <li><a href="#">Sports</a></li>
-                    <li><a href="#">Dieting</a></li>
-                    <li><a href="#">Self Defense</a></li>
-                    <li><a href="#">Meditation</a></li>
-                    <li><a href="#">Mental Health</a></li>
-                    <li><a href="#">Yoga</a></li>
-                    <li><a href="#">Dance</a></li>
-                    <li><a href="#">Other</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="course-grid.html">Photography</a>
-                <ul class="sub-menu">
-                    <li><a href="#">All Photography</a></li>
-                    <li><a href="#">Digital Photography</a></li>
-                    <li><a href="#">Photography Fundamentals</a></li>
-                    <li><a href="#">Commercial Photography</a></li>
-                    <li><a href="#">Video Design</a></li>
-                    <li><a href="#">Photography Tools</a></li>
-                    <li><a href="#">Other</a></li>
-                </ul>
-            </li>
-        </ul>
     </div><!-- end off-canvas-menu -->
-    <div class="mobile-search-form">
-        <div class="d-flex align-items-center">
-            <form method="post" class="flex-grow-1 mr-3">
-                <div class="form-group mb-0">
-                    <input class="form-control form--control pl-3" type="text" name="search" placeholder="Search for anything">
-                    <span class="la la-search search-icon"></span>
-                </div>
-            </form>
-            <div class="search-bar-close icon-element icon-element-sm shadow-sm">
-                <i class="la la-times"></i>
-            </div><!-- end off-canvas-menu-close -->
-        </div>
-    </div><!-- end mobile-search-form -->
+   
     <div class="body-overlay"></div>
 </header><!-- end header-menu-area -->
 <!--======================================
@@ -343,7 +138,7 @@
             <div class="col-lg-7 mx-auto">
                 <div class="card card-item">
                     <div class="card-body">
-                        <h3 class="card-title text-center fs-24 lh-35 pb-4">Login to Your Account!</h3>
+                        <h3 class="card-title text-center fs-24 lh-35 pb-4">Admin Login!</h3>
                         <div class="section-block"></div>
                         <form method="post" class="pt-4">
                             <div class="input-box">
@@ -391,16 +186,14 @@
 <!-- ================================
          END FOOTER AREA
 ================================= -->
-<section class="footer-area pt-100px bg-gray">
+<section class="footer-area pt-100px">
+    <div class="section-block"></div>
     <div class="copyright-content py-4">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6">
                     <div class="d-flex flex-wrap align-items-center">
-                        <a href="index.html" class="pr-4">
-                            <img src="{{ asset('frontend/images/DaDD.png')}}" alt="footer logo" class="footer__logo">
-                        </a>
-                        <p class="copy-desc">Copyright &copy; 2024 <a href="https://sydani.org/">Sydani Technologies Ltd</a>.</p>
+                        <p class="copy-desc">Copyright &copy; 2024 <a href="https://sydani.org/">Sydani Technologies Ltd</a></p>
                     </div>
                 </div><!-- end col-lg-6 -->
                 <div class="col-lg-6">
@@ -409,28 +202,6 @@
                             <li class="mr-3"><a href="#">Terms & Conditions</a></li>
                             <li class="mr-3"><a href="#">Privacy Policy</a></li>
                         </ul>
-                        <div class="select-container select-container-sm">
-                            <select class="select-container-select">
-                                <option value="1">English</option>
-                                <option value="2">Deutsch</option>
-                                <option value="3">Español</option>
-                                <option value="4">Français</option>
-                                <option value="5">Bahasa Indonesia</option>
-                                <option value="6">Bangla</option>
-                                <option value="7">日本語</option>
-                                <option value="8">한국어</option>
-                                <option value="9">Nederlands</option>
-                                <option value="10">Polski</option>
-                                <option value="11">Português</option>
-                                <option value="12">Română</option>
-                                <option value="13">Русский</option>
-                                <option value="14">ภาษาไทย</option>
-                                <option value="15">Türkçe</option>
-                                <option value="16">中文(简体)</option>
-                                <option value="17">中文(繁體)</option>
-                                <option value="17">Hindi</option>
-                            </select>
-                        </div>
                     </div>
                 </div><!-- end col-lg-6 -->
             </div><!-- end row -->
